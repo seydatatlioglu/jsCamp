0b3a198554372f6715cbafc2df01fb9ca64b3b23
seydatatlioglu <statlioglu0@gmail.com>
2021-10-22 18:23:01 +0300

    ilk ders

---
+++ src/app.js
@@ -0,0 +1,23 @@
+
+let dolarD=9
+let dolarB=10
+dolarD="ankara"
+{
+    let dolarD=9.20
+}
+console.log(dolarD)
+
+//array
+
+let krediler=["konut","emlak","ev"]
+console.log(krediler)
+//(3) ['konut', 'emlak', 'ev']
+
+for (let i = 0; i < krediler.length; i++) {
+    const element = krediler[i];
+    console.log(element)   
+    // konut
+    // emlak
+    // ev
+
+}
