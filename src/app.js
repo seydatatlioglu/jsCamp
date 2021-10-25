804e8eca86cc345431ccb5c2d1b25e1299c1b234
seydatatlioglu <statlioglu0@gmail.com>
2021-10-25 12:09:09 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -21,3 +21,24 @@ for (let i = 0; i < krediler.length; i++) {
     // ev
 
 }
+
+let student={id:1,name:"seyda",city:"bolu"} //eğer obje {} tanımı yapılacaksa 'let' kullan...
+console.log(student) //e.ç. {id: 1, name: 'seyda', city: 'bolu'}
+
+let adress={id:2,name:"adres1",city:"izmir"}
+let a={i:2,n:"adres1",c:"izmir"}
+
+/* fonksiyonlar*/
+function save(p,note=20){
+    console.log(p.id+" "+p.name+" "+p.city+" "+note) 
+}
+save(student) //1 seyda bolu 20
+
+function save2(note=20,p){
+    console.log(p.id+" "+p.name+" "+p.city+" "+note) 
+}
+save2(undefined,student) //1 seyda bolu 20
+
+/* array */
+let students=["eda","seda","engin","seyda",student,{id:2,name:"adres1",city:"izmir"}]
+console.log(students) //['eda', 'seda', 'engin', 'seyda']
